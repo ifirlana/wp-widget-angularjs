5d3af865f8b2aff9df04e30d6875fb2256019179
ikhlas firlana <ifirlana@gmail.com>
2016-09-05 08:57:05 +0700

    #1 change base framework

--- app/view_addStock/addStock.js
+++ app/view_addStock/addStock.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.addStock', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+  $routeProvider.when('/addStock', {
+    templateUrl: 'view_addStock/addStock.html',
     controller: 'View2Ctrl'
   });
 }])
