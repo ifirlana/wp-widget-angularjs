ab182a5e39489a820905d693e9d3c83d767dba9b
ikhlas firlana <ifirlana@gmail.com>
2016-09-06 17:25:38 +0700

    #1

--- app/view_viewStock/viewStock.js
+++ app/view_viewStock/viewStock.js
@@ -5,10 +5,19 @@ angular.module('myApp.viewStock', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/viewStock', {
     templateUrl: 'view_viewStock/viewStock.html',
-    controller: 'View1Ctrl'
+    controller: 'ViewStockCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('ViewStockCtrl', ['$scope',function($scope) {
+	console.log("* ViewStockCtrl");
+	//alert("yoo");
+	//fake data 
+	var data_stock = [
+		{"name" : "apple set 12pcs","quantity" : 3, "attributes" : [{"type" : "food"}, {"package" : "plastics"}]},
+		{"name" : "Orange set 6pcs", "quantity" : 4,"attributes" : [{"type" : "food"}]},
+		{"name" : "Viar Motor Cycle 150cc", "quantity" : 1, "attributes" : [{"type" : "vehicle"}, {"status" : "collateral"}]},
+		{"name" : "Toyota Altis 2016 Black v110", "quantity" : 3,"attributes" : [{"type" : "vehicle"}]}];
+	$scope.data_stock = data_stock;
+	$scope.message = "this is stock view";
 }]);
